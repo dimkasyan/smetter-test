c7fa2c96771e0ed033cf25787ca4ff9a17fa7738
Dmitriy Petrov <dartwen@yandex.ru>
2020-07-14 02:03:36 +0300

    Updates

---
+++ precache-manifest.271573fbdae30160f4d6c63faecfa762.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddecd40ad138e8a0ee15a0192709d274",
+    "url": "/smetter-test/index.html"
+  },
+  {
+    "revision": "86656484756d3a006aed",
+    "url": "/smetter-test/static/css/2.bba02b15.chunk.css"
+  },
+  {
+    "revision": "b10a565bd49b999b4058",
+    "url": "/smetter-test/static/css/main.7c721dbc.chunk.css"
+  },
+  {
+    "revision": "86656484756d3a006aed",
+    "url": "/smetter-test/static/js/2.f0477e5f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/smetter-test/static/js/2.f0477e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10a565bd49b999b4058",
+    "url": "/smetter-test/static/js/main.37cbf86c.chunk.js"
+  },
+  {
+    "revision": "3f8ce6e466446ce4b2d0",
+    "url": "/smetter-test/static/js/runtime-main.d3af36ac.js"
+  },
+  {
+    "revision": "4adb0e0182993f47087fde4f5151bb36",
+    "url": "/smetter-test/static/media/diskette.4adb0e01.svg"
+  },
+  {
+    "revision": "7ca482f2dc2983851f84ec99d5075644",
+    "url": "/smetter-test/static/media/loader.7ca482f2.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/smetter-test/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/smetter-test/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/smetter-test/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/smetter-test/static/media/notification.c0d3c94c.eot"
+  }
+]);
